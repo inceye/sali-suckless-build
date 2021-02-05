4b595e155b100138ea68394668a3ac80566da730
Andy Gozas <andy@gozas.me>
2021-02-05 16:13:49 +0000

    Finish the deploy script

--- dwmblocks/config.h
+++ dwmblocks/config.h
@@ -2,34 +2,16 @@
 
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	{"🔊", "pamixer --get-volume-human",				5,			10},
-
-	//KEYBOARD
-	{"🎹", "xkblayout-state print '%n'",	1,		0},
-
-	//RAM
-
-	{"💾", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",	10,		0},
-
-	//CPUTEMP
-	{"🌡", "cputemp",	5,			0},
-
-	//GPUTEMP
-	{"🌡", "gputemp",	5,			0},
-
-	//DISK
-	{"📁/", "df -h / | grep / | awk '{print $4}'",	10,		0},
-
-	{"📁~", "df -h /home/ | grep / | awk '{print $4}'",	10,		0},
-
-	//BATTERY
+	{"🔊", "pamixer --get-volume-human",	5,	10},
+	{"🎹", "xkblayout-state print '%n'",	1,	0},
+	{"💾", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",	10,	0},
+	{"🌡", "cputemp",	5,	0},
+	//{"🌡", "gputemp",	5,	0},
+	{"📁/", "df -h \"/\" | grep \"/\" | awk '{print $4}'",	10,	0},
+	{"📁~", "df -h \"/home\" | grep \"/home\" | awk '{print $4}'",	10,	0},
 	//{"", "cat /etc/status/pipes/battery | cut -d ' ' -f 3",	10,		0},
-
-	//DATE
-	{"📅", "date '+%Y-%m-%d(%a)'",					5,			0},
-
-	//TIME
-	{"⌚", "date '+%I:%M:%S%p'",					1,			0},
+	{"📅", "date '+%Y-%m-%d(%a)'",	5,	0},
+	{"⌚", "date '+%I:%M:%S%p'",	1,	0},
 };
 //Sets delimiter between status commands. NULL character ('\0') means no delimiter.
 static char *delim = " ";
