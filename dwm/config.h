a41bcfaf80cd5442780076724c1807ca6c75ac80
Andy Gozas <andy@gozas.me>
2021-04-24 22:05:17 +0000

    Add ability to change bar height and padding

--- dwm/config.h
+++ dwm/config.h
@@ -15,6 +15,9 @@ static const int swallowfloating    = 0;        /* 1 means swallow floating wind
 static const int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
+static const int user_bh            = 30;       /* 0 means that dwm will calculate bar height, >= 1 means dwm will user_bh as bar height */
+static const int vertpad            = 10;       /* vertical padding of bar */
+static const int sidepad            = 10;       /* horizontal padding of bar */
 static const char *fonts[]          = { "DejaVu Sans Mono:size=13", "JoyPixels:pixelsize=10:antialias=true:autohint=true"  };
 static char dmenufont[]             = "DejaVu Sans Mono:size=13";
 static char normbgcolor[]           = "#003060";
